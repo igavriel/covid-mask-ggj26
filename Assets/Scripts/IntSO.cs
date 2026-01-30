08fc1429ba35a3f20a4247515635743b5bd2643b
ForteGospel <54368132+ForteGospel@users.noreply.github.com>
2026-01-30 13:10:15 +0200

    makstimer and scriptable objects

---
+++ Assets/Scripts/IntSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IntSO", menuName = "Scriptable Objects/IntSO")]
+public class IntSO : ScriptableObject
+{
+    public int Value;
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     [SerializeField] float speed = 500;
+    [SerializeField] float dmgTime = 100;
+    [SerializeField] int maskTimer = 5;
     Rigidbody2D rb2d;
+    bool isMaskOn = false;
+
+    [SerializeField] FloatSO health; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        health.Value = 100;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
@@ -14,5 +20,22 @@ public class playerController : MonoBehaviour
     void Update()
     {
         rb2d.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        if (!isMaskOn) health.Value -= dmgTime * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Mask"))
+        {
+            Destroy(collision.gameObject);
+            isMaskOn = true;
+            Invoke("removeMask", maskTimer);
+
+        }
+    }
+
+    void removeMask()
+    {
+        isMaskOn = false;
     }
 }
