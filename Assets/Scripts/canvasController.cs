97e4fa5ef9db5e7f57ec9c9f157bfa0574e1f177
ForteGospel <54368132+ForteGospel@users.noreply.github.com>
2026-01-30 20:19:47 +0200

    added portrait and canvas

--- Assets/Scripts/canvasController.cs
+++ Assets/Scripts/canvasController.cs
@@ -11,7 +11,8 @@ public class canvasController : MonoBehaviour
     [SerializeField] FloatSO health;
     [SerializeField] FloatSO maskSO;
     [SerializeField] TMP_Text text;
-    [SerializeField] CanvasGroup dmg;
+    [SerializeField] CanvasGroup canvasDMG;
+    [SerializeField] CanvasGroup canvasMask;
 
     Tween dmgFader;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,17 +29,22 @@ public class canvasController : MonoBehaviour
             text.text = MathF.Ceiling(maskSO.Value).ToString();
         else
             text.text = "";
+
+        if (maskSO.Value == 5)
+            canvasMask.DOFade(1, 0.5f);
+        else if (maskSO.Value <= 0)
+            canvasMask.DOFade(0, 0.5f);
     }
 
     public void dmgStart()
     {
-        dmg.DOFade(0.3f, 0.1f);
-        dmgFader = dmg.DOFade(1, 0.3f).SetLoops(-1, LoopType.Yoyo);
+        canvasDMG.DOFade(0.3f, 0.1f);
+        dmgFader = canvasDMG.DOFade(1, 0.3f).SetLoops(-1, LoopType.Yoyo);
     }
 
     public void dmgStop()
     {
         dmgFader.Kill();
-        dmg.DOFade(0, 0.3f);
+        canvasDMG.DOFade(0, 0.3f);
     }
 }
