35f0b2f5609af5a5a7518ab606e5a13b52e5b0e2
ForteGospel <54368132+ForteGospel@users.noreply.github.com>
2026-01-31 16:20:17 +0200

    asd

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class enemyController : MonoBehaviour
 {
     Quaternion fixedRotation;
+    [SerializeField] bool isWalking;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        fixedRotation = Quaternion.Euler(0,0,90f);
+        
+        fixedRotation = Quaternion.Euler(0f, transform.parent.rotation.eulerAngles.y, 0f);
+        GetComponent<Animator>().SetBool("isWalking", isWalking);
     }
 
     // Update is called once per frame
